f4dc11aa6ec469b6950e666ba4498934f4e45bea
DizMaxWork <sol.work@mail.ru>
2023-09-14 15:29:44 +0300

    first commit

---
+++ module_4/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Name from './components/Name';
+
+function App() {
+  return (
+    <div>
+      <Name />
+    </div>
+  );
+}
+
+export default App;
